cb132a11e8a1e75fa57c115b4cd5fa447fb2c5f3
Tharushika Dilakshan <101235785+TharuZika@users.noreply.github.com>
2024-10-25 23:25:46 +0530

    some changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,6 @@ const { registerUser, loginUser } = require('../controllers/authController');
 const { registerValidationRules, loginValidationRules } = require('../util/authValidator'); // Import validation rules
 const router = express.Router();
 
-// Apply validation middleware to the register and login routes
 router.post('/register', registerValidationRules(), registerUser);
 router.post('/login', loginValidationRules(), loginUser);
 
